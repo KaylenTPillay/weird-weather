60ce8dffebee8892e9a8bd810ab5a2bbd72f85c8
Kaylen Travis Pillay <kaylen.pillay@softwareological.com>
2019-03-20 23:00:38 +0200

    Added the Dockerfile and some started code to run Hello World

---
+++ weird-weather/app.py
@@ -0,0 +1,13 @@
+"""Weird-Weather.
+This application fetches the latest weather data from the internet and
+displays that information from the user. Depending on the given location
+of the user, he/she will receive relevant information pertaining to said
+location.
+
+Authors: [ Darshan Pillay, Kaylen Pillay ]
+"""
+def __helloWorld():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    __helloWorld()
